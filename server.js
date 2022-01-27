f8803fc6b4a324c12d395fb50319a0472c0377a8
Tushar Gupta <tushargupta1999@gmail.com>
2022-01-27 23:13:59 +0530

    initail commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("express")();
+
+const port = process.env.port || 5000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get("/data", (req, res) => {
+    res.json({"id":1, "data":"hello world"});
+});
+
+app.listen(port, console.log(`Listening on port ${port}`));
